0763551ed83c83b3a1601bfe87d9b4df86850289
Jeff Coleman <jeff@jeffcolemanwrites.com>
2019-02-06 06:58:17 -0800

    now disabling form fields during ajax call with a clear loading message

--- view/frontend/web/js/newsletter-ajax.js
+++ view/frontend/web/js/newsletter-ajax.js
@@ -15,6 +15,24 @@ define([
 
 	return function (config, elements) {
 
+		// Enable the subscription input and submission elements once an AJAX
+		// subscription request is complete (or there was an error.)
+		function enableSubscription() {
+
+			$(elements).find('input[type="email"]').attr('disabled', false);
+			$(elements).find('.action.subscribe').attr('disabled', false);
+			$(elements).find('.newsletter-status').remove();
+		}
+
+		// Disable the subscription input and submission elements while submitting
+		// an AJAX subscription request.
+		function disableSubscription() {
+
+			$(elements).find('input[type="email"]').attr('disabled', true);
+			$(elements).find('.action.subscribe').attr('disabled', true);
+			$(elements).find('.content').append('<div class="newsletter-status">Subscribing&nbsp;&nbsp;&nbsp;<img src="' + config.loadingImgUrl + '" /></div>');
+		}
+
 		let popup = $('#newsletter-status-modal').modal({
 			modalClass: 'newsletter-status-modal',
 			buttons: {}
@@ -26,6 +44,8 @@ define([
 
 		$(elements).find('form').submit(function (e) {
 
+			disableSubscription();
+
 			if ($(this).validation('isValid')) {
 
 				$.post(
@@ -33,11 +53,13 @@ define([
 					{email: $(this).find('[name="email"]').val()},
 					function (response) {
 						$('#newsletter-status-modal').find('.content').text(response.message);
+						enableSubscription();
 						popup.modal('openModal');
 					}
 				).fail(
 					function (response) {
 						$('#newsletter-status-modal').find('.content').text('An error occurred while trying to subscribe. Please try again.');
+						enableSubscription();
 						popup.modal('openModal');
 					}
 				);
