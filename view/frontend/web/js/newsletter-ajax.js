5a25a8b3938b69ac784b8ebda11c273f8ae0dd73
Jeff Coleman <jeff@jeffcolemanwrites.com>
2019-03-12 17:55:04 -0700

    refactored requirejs module so that it's extendable

--- view/frontend/web/js/newsletter-ajax.js
+++ view/frontend/web/js/newsletter-ajax.js
@@ -13,62 +13,90 @@ define([
 
 	'use strict';
 
-	return function (config, elements) {
+	return {
 
 		// Enable the subscription input and submission elements once an AJAX
 		// subscription request is complete (or there was an error.)
-		function enableSubscription() {
+		enableSubscription: function () {
 
-			$(elements).find('input[type="email"]').attr('disabled', false);
-			$(elements).find('.action.subscribe').attr('disabled', false);
-			$(elements).find('.newsletter-status').remove();
-		}
+			$(this.elements).find('input[type="email"]').attr('disabled', false);
+			$(this.elements).find('.action.subscribe').attr('disabled', false);
+			$(this.elements).find('.newsletter-status').remove();
+		},
 
 		// Disable the subscription input and submission elements while submitting
 		// an AJAX subscription request.
-		function disableSubscription() {
+		disableSubscription: function () {
 
-			$(elements).find('input[type="email"]').attr('disabled', true);
-			$(elements).find('.action.subscribe').attr('disabled', true);
-			$(elements).find('.content').append('<div class="newsletter-status">Subscribing&nbsp;&nbsp;&nbsp;<img src="' + config.loadingImgUrl + '" /></div>');
+			$(this.elements).find('input[type="email"]').attr('disabled', true);
+			$(this.elements).find('.action.subscribe').attr('disabled', true);
+			$(this.elements).find('.content').append('<div class="newsletter-status">Subscribing&nbsp;&nbsp;&nbsp;<img src="' + this.config.loadingImgUrl + '" /></div>');
+		},
+
+		// Called after a successful post (doesn't necessarily mean the user was
+		// successfully subscribed.)
+		postSuccess: function (response) {
+
+			$('#newsletter-status-modal').find('.content').text(response.message);
+			this.enableSubscription();
+			this.popup.modal('openModal');
+		},
+
+		// An error occurred during the post request.
+		postError: function (response) {
+
+			$('#newsletter-status-modal').find('.content').text('An error occurred while trying to subscribe. Please try again.');
+			this.enableSubscription();
+			this.popup.modal('openModal');
+		},
+
+		// Called when the user clicks the "Subscribe" button.
+		subscribeAction: function (e) {
+
+			var that = this;
+			this.disableSubscription();
+
+			$.post(
+				'/newsletter/subscriber/newajax',
+				{email: $(this.elements).find('[name="email"]').val()},
+				function (response) {
+					that.postSuccess(response);
+				}
+			).fail(
+				function (response) {
+					that.postError(response);
+				}
+			);
+		},
+
+		// Initialization method
+		newsletterAjax: function (config, elements) {
+
+			var that = this;
+
+			this.config = config;
+			this.elements = elements;
+
+			this.popup = $('#newsletter-status-modal').modal({
+				modalClass: 'newsletter-status-modal',
+				buttons: {}
+			});
+
+			$('#newsletter-status-modal .continue-shopping').on('click', function (e) {
+				that.popup.modal('closeModal');
+			});
+
+			$(elements).find('form').submit(function (e) {
+
+				if ($(this).validation('isValid')) {
+					that.subscribeAction(e);
+				}
+
+				// Suppress the original form submission since we're doing an AJAX
+				// request instead.
+				e.preventDefault();
+			});
 		}
-
-		let popup = $('#newsletter-status-modal').modal({
-			modalClass: 'newsletter-status-modal',
-			buttons: {}
-		});
-
-		$('#newsletter-status-modal .continue-shopping').on('click', function (e) {
-			popup.modal('closeModal');
-		});
-
-		$(elements).find('form').submit(function (e) {
-
-			disableSubscription();
-
-			if ($(this).validation('isValid')) {
-
-				$.post(
-					'/newsletter/subscriber/newajax',
-					{email: $(this).find('[name="email"]').val()},
-					function (response) {
-						$('#newsletter-status-modal').find('.content').text(response.message);
-						enableSubscription();
-						popup.modal('openModal');
-					}
-				).fail(
-					function (response) {
-						$('#newsletter-status-modal').find('.content').text('An error occurred while trying to subscribe. Please try again.');
-						enableSubscription();
-						popup.modal('openModal');
-					}
-				);
-			}
-
-			// Suppress the original form submission since we're doing an AJAX
-			// request instead.
-			e.preventDefault();
-		});
 	};
 });
 
